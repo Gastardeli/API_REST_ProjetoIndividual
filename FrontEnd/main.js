00e35630ac21185ea5e22cb3b1af3d22f11051d4
Gastardeli <victorgastardeli11@gmail.com>
2026-08-30 15:37:33 -0300

    Primeira versão de fetch de GET e POST

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -9,20 +9,60 @@ function validacaoCampos() {
 
     const descricao = document.getElementById("descricao")
 
+    const select = document.getElementById("perfil_input").value
+
     if (agora > dataCliente) {
         alert("A data selecionada é inválida!")
-        return
     } else if (valor <= 0 || valor > 100000) {
         alert("Coloque um valor válido!")
-        return
     } else if (nome == null || nome.trim() === "" || descricao.trim() === "" || descricao == null) {
         alert("Atenção certifique-se de que os campos estão preenchidos")
-        return
+    } else if (select == "") {
+        alert("Certifique-se de que selecionou uma opção!")
     } else {
-        enviarDados()
+        enviarDados(data, valor, nome, descricao, select)
     }
 }
 
-function enviarDados(){
+async function buscarSelect() {
+    try {
+        const resposta = await fetch('http://localhost:8080/pecas/select')
 
+        if (!resposta.ok) {
+            throw new Error('Error na requisição: ' + resposta.status)
+        }
+
+        const dadosSelect = await resposta.json()
+    } catch (erro) {
+        console.log('Erro ao pegar dados do select: ' + erro)
+    }
+}
+
+async function enviarDados(data, valor, nome, descricao, select) {
+    const novaPeca = { 
+        nome: nome, 
+        valor: valor,
+        descricao: descricao,
+        data: data,
+        select: select 
+    };
+
+    try {
+        const resposta = await fetch('https://localhost:8080/pecas', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(novaPeca)
+        });
+
+        if (!resposta.ok) {
+            throw new Error('Erro ao enviar dados');
+        }
+
+        const resultado = await resposta.json();
+        console.log('Sucesso:', resultado);
+    } catch (erro) {
+        console.error('Erro:', erro);
+    }
 }
